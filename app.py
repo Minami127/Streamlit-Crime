c2fef424bfffbf8c9ecbb88231ab8770dc88ec31
Minami127 <jws282288@gmail.com>
2023-11-23 16:42:25 +0900

    project

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import seaborn as sb
 
 def main() :
     st.header('경찰청 인천광역시경찰청_관서별 5대범죄 112신고건수 현황')
-    df=pd.read_csv('')
+    df=pd.read_csv('C:\Users\40005\Documents\GitHub\Streamlit-Crime\data\crime.csv')
     st.dataframe(df)
 
 if __name__ == '__main__' :
