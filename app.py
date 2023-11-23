09465268d11cd104943dd282dc66dc33413aec59
Minami127 <jws282288@gmail.com>
2023-11-23 15:53:16 +0900

    project

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+def main() :
+    st.header('경찰청 인천광역시경찰청_관서별 5대범죄 112신고건수 현황')
+    df=pd.read_csv('')
+    st.dataframe(df)
+
+if __name__ == '__main__' :
+    main()
