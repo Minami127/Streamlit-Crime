a4ed5f4bbacb6aa3a71ec312c934fa89d453bfaf
Minami127 <jws282288@gmail.com>
2023-11-24 17:10:58 +0900

    ㄴ

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 import streamlit as st
-from app_eda import run_eda_app
+import matplotlib.pyplot as plt
 from app_home import run_home_app
+from app_eda import run_eda_app
+
+import platform
+from matplotlib import font_manager, rc
+plt.rcParams['axes.unicode_minus'] = False
+if platform.system() == 'Linux':
+    rc('font', family='NanumGothic')
 
 
 def main() :
-    st.title('경찰청 인천광역시경찰청 관서별 5대범죄 112신고건수 현황')
+    st.title('경찰청 112신고건수 현황')
 
     menu=['Home','EDA','ML']
 
     choice = st.sidebar.selectbox('메뉴선택', menu)
 
-   
-    
 
     if choice == menu[0] :
         run_home_app()
     elif choice == menu[1] :
-       run_eda_app()
+        run_eda_app()
     elif choice == menu[2] :
+        pass
+    elif choice == menu[3] :
         pass
 
    
--- app_eda.py
+++ app_eda.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sb
-
+import plotly.express as px
 
 def run_eda_app () :
+     
      st.subheader('통계데이터 확인하기')
     
      df=pd.read_csv('./data/crime.csv', encoding='euc-kr')
@@ -19,21 +17,42 @@ def run_eda_app () :
           st.dataframe(df.describe())
      else :
           st.text('')
-    
-
-     st.info('지역별 신고건수')
+     
+     radiohead=['지역별 신고건수', '가장 많이 들어오는 신고건수','가장 적게 들어오는 신고건수']
      
      fixed_column = '범죄 분류'
-     selected_region = st.selectbox('',df.columns[1:])
-     selected_data = df[[fixed_column,selected_region]]
-     selected_data = df[[fixed_column, selected_region]].transpose()
-     #height = min(len(selected_data.columns) * 20, 300)
-     st.dataframe(selected_data,height= 110,width=700)
 
      
 
+     st.info('분류별로 보기')
 
+     radio = st.radio('',radiohead)
+     pd.set_option('display.max_colwidth', None)
+
+     if radio == radiohead[0] :
+          selected_region = st.selectbox('',df.columns[1:])
+          selected_data = df[[fixed_column,selected_region]]
+          selected_data = df[[fixed_column, selected_region]].transpose()
+          st.dataframe(selected_data)
+     elif radio == radiohead[1] :
+          column_list=df.columns[1 : ]
+          selected_column=st.selectbox('', column_list)
+          top5_selected_column_df = df.nlargest(5, selected_column)[[fixed_column, selected_column]]
+          st.dataframe(top5_selected_column_df)
+          fig1 = px.pie(top5_selected_column_df, values=selected_column, names=fixed_column, title=f'{selected_column}의 상위 5개 신고건수 ')
+          st.plotly_chart(fig1)
+     elif radio == radiohead[2] :
+          column_list=df.columns[1 : ]
+          selected_column=st.selectbox('', column_list)
+          bottom5_selected_column_df = df.nsmallest(5, selected_column)[[fixed_column, selected_column]]
+          st.dataframe(bottom5_selected_column_df)
+          fig2 = px.pie(bottom5_selected_column_df, values=selected_column, names=fixed_column, title=f'{selected_column}의 하위 5개 신고건수')
+          st.plotly_chart(fig2)
+          
+
+
+     st.header('지역별 신고건수 비교하기')
 
 
 if __name__ == '__main__' :
-    main()
+     main()
